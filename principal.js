ecde3cc901a0b4b07989963f293e329990e09a92
Andre Dornas <andredornas299@gmail.com>
2016-04-10 13:47:52 -0300

    commit final

--- principal.js
+++ principal.js
@@ -11,4 +11,25 @@
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+var rolarDado = function(valor, quantidade) {
+  var soma = 0;
+  for(var i = 0; i < quantidade; i++){
+    soma += Math.ceil(Math.random() * valor);
+  }
+  return soma;
+}
 
+document.getElementById('rolar').addEventListener('click', function(){
+  var valores = [4,6,8,10,12,20],
+      total = 0;
+  valores.forEach(function(val){
+    var id = 'quantidadeD'+val;
+    console.log(id);
+    var qnt = document.getElementById(id).value;
+    console.log("val = " + val + "  qnt = " + qnt);
+    total += rolarDado(val, qnt);
+  });
+  console.log(total);
+  document.getElementById('recipienteResultados').classList.remove('oculto');
+  document.getElementById('resultado').innerHTML = total;
+});
